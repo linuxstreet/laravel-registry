e2267fff3cbaf168fc946c2e2d0e3837277d7e7c
Igor Jovanovic <linuxstreet@gmail.com>
2018-11-13 18:25:45 +0100

    StyleCI fix

--- src/routes.php
+++ src/routes.php
@@ -14,18 +14,18 @@ Route::group($routeConfig, function () {
     Route::post(config('registry.settings.path'), 'RegistryController@store')
         ->name('registry.store');
 
-    Route::get(config('registry.settings.path').'/create', 'RegistryController@create')
+    Route::get(config('registry.settings.path') . '/create', 'RegistryController@create')
         ->name('registry.create');
 
-    Route::get(config('registry.settings.path').'/{registry}', 'RegistryController@show')
+    Route::get(config('registry.settings.path') . '/{registry}', 'RegistryController@show')
         ->name('registry.show');
 
-    Route::patch(config('registry.settings.path').'/{registry}', 'RegistryController@update')
+    Route::patch(config('registry.settings.path') . '/{registry}', 'RegistryController@update')
         ->name('registry.update');
 
-    Route::delete(config('registry.settings.path').'/{registry}', 'RegistryController@destroy')
+    Route::delete(config('registry.settings.path') . '/{registry}', 'RegistryController@destroy')
         ->name('registry.destroy');
 
-    Route::get(config('registry.settings.path').'/{registry}/edit', 'RegistryController@edit')
+    Route::get(config('registry.settings.path') . '/{registry}/edit', 'RegistryController@edit')
         ->name('registry.edit');
 });
